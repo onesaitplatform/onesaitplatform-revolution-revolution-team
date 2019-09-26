888de81b06aa778983d5f8890c4789d15ca3fc29
Rafael San Miguel Carrasco <rsanmiguel@minsait.com>
2019-09-26 23:44:28 +0100

    version 1 ready

--- backend/microsrv.py
+++ backend/microsrv.py
@@ -1,21 +1,7 @@
 
-# Imports
-from flask import Flask, request, redirect, url_for
-import os
-import hashlib
-import datetime
-import csv
-import pyffx
-
 # Includes
+exec(open("lib.py").read())
 exec(open("tokenizer.py").read())
-
-# Parameters
-path_upload = 'uploads'
-path_download = 'downloads'
-error = 'error'
-default_sep = ','
-newline = '\n'
 
 # Init
 app = Flask(__name__)
@@ -35,8 +21,8 @@ def save_file(
         download_link)
 
 # CSV
-def csv_reader(f):
-    return(csv.reader(f, quotechar = '"', delimiter = default_sep,
+def csv_reader(f, separator):
+    return(csv.reader(f, quotechar = '"', delimiter = separator,
         quoting = csv.QUOTE_ALL, skipinitialspace = True))
 
 # Hash
@@ -52,9 +38,32 @@ def get_id(s):
 def hello():
     return 'hello'
 
+# Jobs
+@app.route('/jobs',
+    methods = ['GET'])
+def jobs():
+    user = request.args.get(
+        'user', default = None, type = str)
+    jobs = list(query_ontology(
+        config['ontology_jobs'], { 'user': user }))
+    json = { 'user': user, 'jobs': jobs }
+    return(json)
+
+# Secret
+@app.route('/secret',
+    methods = ['GET'])
+def secret():
+    id = request.args.get(
+        'id', default = None, type = str)
+    f = open(path_secrets + '/' + id)
+    secret = f.readlines()
+    json = { 'id': id,
+        'secret': secret }
+    return(json)
+
 # Tokenize: line
 def tokenize_line(
-        flags, tokenizer, line):
+        flags, tokenizer, line, separator):
     line_tokenized = list()
     for i, value in enumerate(line):
         if (flags[i] == 1):
@@ -62,7 +71,7 @@ def tokenize_line(
                 tokenizer.tokenize(value))
         else: line_tokenized.append(
             value)
-    return(default_sep.join(
+    return(separator.join(
         line_tokenized))
 
 # Tokenize
@@ -71,19 +80,22 @@ def tokenize_line(
 def tokenize():
     id = request.args.get(
         'id', default = None, type = str)
+    separator = list(query_ontology(
+        config['ontology_jobs'],
+            { 'id': id }))[0]['separator']
     flags = request.json['flags']
     tokenizer = Tokenizer(
         request.json['method'], id)
     output = list()
     f = open(path_upload + '/' + id)
-    for i, line in enumerate(csv_reader(f)):
+    for i, line in enumerate(csv_reader(f, separator)):
         if (i == 0):
             output.append(
                 ''.join(line))
         line_tokenized = tokenize_line(
-            flags, tokenizer, line)
+            flags, tokenizer, line, separator)
         output.append(
-            line_tokenized)
+            line_tokenized + newline)
     return(save_file(
         id, output))
 
@@ -93,13 +105,15 @@ def tokenize():
 def extract_fields():
     id = request.args.get(
         'id', default = None, type = str)
+    separator = query_ontology(
+        collection, { 'id': id })[separator]
     f = open(UPLOAD_FOLDER + '/' + id)
     fields = f.readline()
     fields_list = fields.split(
-        default_sep)
+        separator)
     values = f.readline()
     values_list = values.split(
-        default_sep)
+        separator)
     json = { 'id': id,
         'fields': fields_list,
         'values': values_list }
@@ -111,13 +125,28 @@ def extract_fields():
 def upload_file():
     if request.method != 'POST':
         return(error)
+    user = request.args.get(
+        'user', default = anonymous_user, type = str)
+    separator = request.args.get(
+        'separator', default = None, type = str)
     if 'file' in request.files:
         file = request.files['file']
         if file.filename != '':
             filename = file.filename
             filename_id = get_id(filename)
-            file.save(os.path.join(
-                app.config['UPLOAD_FOLDER'], filename_id))
+            file_path = os.path.join(
+                app.config['UPLOAD_FOLDER'], filename_id)
+            file.save(file_path)
+            num_lines = sum(
+                1 for line in open(file_path))
+            now = datetime.datetime.now()
+            insert_ontology_single(
+                dict(user = user, id = filename_id,
+                    filename = filename, separator = separator,
+                    timestamp = now, hour = now.hour,
+                    weekday = now.strftime('%A'),
+                    num_lines = num_lines),
+                config['ontology_jobs'])
             return(filename_id)
     return(error)
 
--- backend/tokenizer.py
+++ backend/tokenizer.py
@@ -4,24 +4,73 @@ class Tokenizer:
     # Attributes
     secret = None
     method = None
-    methods = [ 'FPE' ]
-    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=-_.:,;{}[]"¡!¿?+*|\'é'
+    engine = None
+    methods = [ 'FPE', 'AES', 'MAP' ]
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\''
     default_char = '?'
+    utf8_encoding = 'utf-8'
+
+    def save_secret(self, id):
+        f = open(path_secrets + '/' + id, 'w+')
+        f.writelines(str(
+            self.secret))
+        return
 
     def __init__(self, method, id):
         self.method = method
-        self.secret = (str(
-            datetime.datetime.now().timestamp()) + '-' + id).encode()
+        if (method == self.methods[0]):
+            self.secret = (str(
+                datetime.datetime.now().timestamp()) + '-' + id).encode()
+        elif (method == self.methods[1]):
+            self.secret = Fernet.generate_key()
+            self.engine = Fernet(self.secret)
+        elif (method == self.methods[2]):
+            self.engine = dict()
+            alphabet_list = list(self.alphabet)
+            random.shuffle(alphabet_list)
+            alphabet_map = ''.join(
+                alphabet_list)
+            self.secret = self.alphabet + \
+                newline + alphabet_map
+            for i, new_ch in enumerate(alphabet_map):
+                self.engine[self.alphabet[i]] = new_ch
+        self.save_secret(id)
+        return
 
     def tokenize(self, value):
         value = value.encode(
             'ascii', errors = 'ignore')
         token = ''
         if (self.method == self.methods[0]):
-            tokenize_fpe(value)
+            token = self.tokenize_fpe(value)
+        elif (self.method == self.methods[1]):
+            token = self.tokenize_aes(value)
+        elif (self.method == self.methods[2]):
+            token = self.tokenize_map(value)
         return(token)
 
+    def tokenize_aes(self, value):
+        value = str(value).encode()
+        value_enc = self.engine.encrypt(value)
+        value_enc = value_enc.decode(
+            self.utf8_encoding)
+        return(value_enc)
+
+    def tokenize_map(self, value):
+        value = str(value).encode()
+        value = value.decode(
+            self.utf8_encoding)
+        value_enc = ''
+        for ch in value:
+            if (ch in self.alphabet):
+                value_enc += self.engine[ch]
+            else: value_enc += self.default_char
+        return(value_enc)
+
     def tokenize_fpe(self, value):
+        token = ''
+        value = value.decode(
+            self.utf8_encoding)
         # String
         if (type(value) is str):
             pending = True
