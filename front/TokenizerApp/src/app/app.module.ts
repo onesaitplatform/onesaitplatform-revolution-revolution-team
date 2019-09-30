75e21be2cee033ea7f913dd5b57c9900853a5644
sirtxo <39530868+sirtxo@users.noreply.github.com>
2019-09-30 16:15:33 +0200

    RT_TOKENIZER_FRONT-> create services to do the REST aplication in front
    i don't can test because https://www.onesaitplatform.online/controlpanel/login is down

--- front/TokenizerApp/src/app/app.module.ts
+++ front/TokenizerApp/src/app/app.module.ts
@@ -2,19 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterModule, Routes } from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle'; 
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { HttpClientModule } from '@angular/common/http';
-import {MatTableModule} from '@angular/material/table'; 
+import {MatTableModule} from '@angular/material/table';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatSelectModule} from '@angular/material/select'; 
-import {MatCheckboxModule} from '@angular/material/checkbox'; 
-import {MatRadioModule} from '@angular/material/radio'; 
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
 
 
 import { AppComponent } from './app.component';
@@ -63,10 +63,11 @@ const appRoutes: Routes = [
     MatButtonModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } 
+      { enableTracing: true }
     )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- front/TokenizerApp/src/app/login/login.component.ts
+++ front/TokenizerApp/src/app/login/login.component.ts
@@ -1,33 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
-
-
+import { environment } from '../../environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import 'rxjs-compat/add/operator/map';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 
-
+@Injectable()
 export class LoginComponent implements OnInit {
-
+  env = environment;
   username: string;
   psw: string;
-  submitLogin:boolean = false;
+  submitLogin = false;
 
-  constructor(private router:Router ) { }
+  constructor(private router: Router , private http: HttpClient ) { }
 
   ngOnInit() {
   }
 
-  login(){
+  login() {
     this.submitLogin = true;
-    if (this.username && this.psw)
-    this.router.navigate(['/core']);
+    if (this.username && this.psw) {
+      if (this.doLogin()) {
+        this.router.navigate(['/core']);
+      } else {
+        alert('Login isn\'t correct');
+      }
+    }
+  }
+  private doLogin() {
+    this.env.token = '';
+    this.env.userName = this.username;
+    const formData = new FormData();
+    formData.append('grant_type', 'password');
+    formData.append('username', this.username);
+    formData.append('password', this.psw);
+
+    const httpOptions = {
+        headers: new HttpHeaders({
+        'Authorization': 'Basic b25lc2FpdHBsYXRmb3JtOm9uZXNhaXRwbGF0Zm9ybQ==',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      })
+    };
+
+    this.http.post('https://www.onesaitplatform.online/oauth-server/oauth/token', formData, httpOptions).subscribe(
+      ( res: Response) => {
+      this.env.token = res.headers.get('access_token');
+     }
+    );
+    return false;
   }
 
-  skipGuess(){
- 
+  skipGuess() {
     this.router.navigate(['/core']);
   }
 
