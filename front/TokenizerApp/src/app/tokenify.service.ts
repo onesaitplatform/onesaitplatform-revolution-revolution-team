05f79fdf9d0bcc17429d01295b16ea8ed9887763
sirtxo <39530868+sirtxo@users.noreply.github.com>
2019-10-03 10:09:06 +0200

    tokenify service

---
+++ front/TokenizerApp/src/app/tokenify.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenifyService {
+  private resultado: Object;
+
+  constructor(protected http: HttpClient) { }
+
+  async putTokenifyLinks(idFile: String, flags: String , method: String) {
+    // TODO quit with real values
+    flags = '[0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0]';
+    idFile = '5d94a4a918b39b000cf1bfd6';
+    method = 'FPE';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // tslint:disable-next-line:max-line-length
+        'Authorization': 'Bearer ' + environment.token,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      })
+    };
+    const body = new FormData();
+    body.append('params',
+      '"USER_TOKEN": "Bearer "' + environment.token + ',"file_id": "' + idFile + '",' +
+      '"user": "anonymous","method": "' + method + '","flags": ' + flags );
+    await this.http.post(
+      environment.uploadUrl , body , httpOptions
+    ).subscribe(
+      res => {
+        this.resultado = res;
+        console.log('Correct: links obtained successful ');
+
+      }, err => {
+        this.resultado = null;
+        console.log(err);
+      }
+    );
+  }
+
+  async listTokenifyFields(idFile: String) {
+    // TODO quit with real values
+    idFile = '5d94a4a918b39b000cf1bfd6';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // tslint:disable-next-line:max-line-length
+        'Authorization': 'Bearer ' + environment.token,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      })
+    };
+    const body = new FormData();
+    body.append('params',
+      '"USER_TOKEN": "Bearer "' + environment.token + ',' +
+      '"user": "anonymous","separator": ",","file_id": ' + idFile );
+    await this.http.post(
+      environment.uploadUrl , body , httpOptions
+    ).subscribe(
+      res => {
+        this.resultado = res;
+        console.log('Correct: links obtained successful ');
+
+      }, err => {
+        this.resultado = null;
+        console.log(err);
+      }
+    );
+  }
+
+}
